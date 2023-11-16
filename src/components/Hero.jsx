f34ffb54112f0b9aace79dae0e68a71b6e30d26c
Ihsan Tri Marseno <ihsanmarseno09@gmail.com>
2023-11-16 13:06:41 +0700

    add hero

---
+++ src/components/Hero.jsx
@@ -0,0 +1,24 @@
+
+const Hero = () => {
+  return (
+    <div className='text-white'>
+      <div className='max-w-[800px] mt-[-96px] w-full h-screen mx-auto text-center flex flex-col justify-center'>
+        <p className='text-[#00df9a] font-bold p-2'>
+          GROWING WITH DATA ANALYTICS
+        </p>
+        <h1 className='text-4xl font-bold md:text-7xl sm:text-6xl md:py-6'>
+          Grow with data.
+        </h1>
+        <div className='flex items-center justify-center'>
+          <p className='py-4 text-xl font-bold md:text-5xl sm:text-4xl'>
+            Fast, flexible financing for BTB, BTC, SASS
+          </p>
+        </div>
+        <p className='text-xl font-bold text-gray-500 md:text-2xl'>Monitor your data analytics to increase revenue for BTB, BTC, & SASS platforms.</p>
+        <button className='bg-[#00df9a] w-[200px] rounded-md font-medium my-6 mx-auto py-3 text-black'>Get Started</button>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
